8c2f85d60d69384236b68b133bbbf4aec41c4360
Josh Bird <joshua.thomas.bird@gmail.com>
2019-01-14 14:50:56 -0500

    fetching fixed

--- fetch.py
+++ fetch.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+from ipdb import set_trace as st
 
 from elasticsearch import Elasticsearch
 from requests import post
@@ -8,7 +10,7 @@ from itertools import chain
 
 
 records = []
-request_body = {
+ucsd_request_body = {
     "sort": {
         "date_created": {
             "order": "asc"
@@ -275,36 +277,98 @@ request_body = {
     }
 }
 
+asapbio_request_body = {
+    "sort": {
+        "date_created": {
+            "order": "asc"
+        }
+    },
+    "size": 5000,
+    "query": {
+        "bool": {
+            "filter": [
+                {
+                    "bool": {
+                        "should": [
+                            {
+                                "term": {
+                                    "type": "preprint"
+                                }
+                            },
+                        ]
+                    }
+                }
+            ],
+            "must": [
+                {
+                    "query_string": {
+                        "query": "*"
+                    }
+                }
+            ]
+        }
+    }
+}
+
 
 # Fetch records in recent half
+request_body  = asapbio_request_body
 
 request_body['sort']['date_created']['order'] = 'desc'
 
+record_dict = {}
 
-try:
-    records = records + post(
+total = None
+
+while total is None or len(records) < total:
+    if total is not None:
+        print(f"{len(record_dict)} unique of {len(records)} retrieved from {total} total records.")
+        request_body['search_after'] = records[-1]['sort']
+    else:
+        print("Beginning record retrieval")
+    response = None
+    print("Making request.")
+    response = post(
         'https://share.osf.io/api/v2/search/creativeworks/_search',
         headers={
             'Content-Type': 'application/json'
         },
         data=dumps(request_body)
-    ).json()['hits']['hits']
-except:
-    import ipdb; ipdb.set_trace()
+    ).json()
+    print("Request successful.")
+    try:
+        records = records + response['hits']['hits']
+        total = response['hits']['total']
+        record_dict = {}
+
+        for record in records:
+            record_dict[record['_id']] = record
+
+        with open('./asapbio.json', 'w') as file:
+            file.write(dumps(records))
+    except:
+        print(response.keys())
+        break
+
+print('# Press ctrl-d to exit without writing the data to a file')
+import ipdb; ipdb.set_trace()
 
 
-# Fetch reocords in early half
+sys.exit()
 
+import ipdb; ipdb.set_trace()
 request_body['sort']['date_created']['order'] = 'asc'
 
+res = None
+
 try:
-    records = records + post(
+    res = post(
         'https://share.osf.io/api/v2/search/creativeworks/_search',
         headers={
             'Content-Type': 'application/json'
         },
         data=dumps(request_body)
-    ).json()['hits']['hits']
+    )
 except:
     import ipdb; ipdb.set_trace()
 
@@ -319,18 +383,30 @@ record_dict = {}
 for record in records:
     record_dict[record['_id']] = record
 
-print(record_dict.values())
+print(len(record_dict))
 
-
+import sys
+sys.exit()
 
 # Records deduplicated
 ###############################################################################
 # Upload records to UCSD elastic cluster
 
-ucsd_elastic_cluster = Elasticsearch([
-    'https://elastic:pJBjKf1HFVXaJYKEABXicHx4@https://a99f2969ba11fbde063dfcd46130d10e.us-east-1.aws.found.io:9243'
-],
-    use_ssl=True
+
+ucsd_elastic_cluster = Elasticsearch(
+    ['https://dev-labs.cos.io'],
+    use_ssl=True,
+    retry_on_timeout=True
 )
+
+
+for record in records:
+
+    res = ucsd_elastic_cluster.index(
+        index="records",
+        doc_type="record",
+        id=record["_id"],
+        body=record["_source"]
+    )
 
 
